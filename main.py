def52be7d64347b63b9a3571a79f6de83e38ac1c
Александр Щукин <shykin003@gmail.com>
2025-05-01 17:21:30 +0300

    Initial commot

---
+++ main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI, UploadFile, File
+from fastapi.responses import JSONResponse
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+import io
+import  cv2
+
+app = FastAPI()
+model = tf.keras.models.load_model('best_model.h5')
+
+def preprocess_image(img):
+    image = img.copy()
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    image = cv2.resize(image, (150, 150))
+    image = image.reshape((1, 150, 150, 3))
+    image = np.array(image, dtype="float32") / 255.0
+    return image
+
+@app.post("/predict")
+async def predict(file: UploadFile = File(...)):
+    contents = await file.read()
+    image = Image.open(io.BytesIO(contents))
+    processed_image = preprocess_image(image)
+    predictions = model.predict(processed_image)
+    predicted_class = int(np.argmax(predictions[0]))
+    probabilities = predictions[0].tolist()
+    return JSONResponse(content={"predicted_class": predicted_class, "probabilities": probabilities})
+
